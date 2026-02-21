54599c6120b118e9ff11189503a9d6101e16afb9
Harsh Shukla <harshshukla24112000@gmail.com>
2026-02-21 20:40:18 +0530

    authorise

--- Authentication-Session/db/schema.js
+++ Authentication-Session/db/schema.js
@@ -1,8 +1,10 @@
-import { uuid, pgTable, varchar } from "drizzle-orm/pg-core";
+import { uuid, pgTable, varchar, text } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  password:text().notNull()
+  password: text().notNull(),
+  salt: text().notNull(),
 });
+
